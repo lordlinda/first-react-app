18538e4c135384d76e55f3c99746a8335817d5f8
lordlinda <marialindananono@gmail.com>
2020-04-24 10:52:11 +0300

    Updates

---
+++ precache-manifest.29074f8d0d5297fb6f189efd73e3af95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3e3d2187c1234d9dd594bc31c3f23e",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "2d50065ed4e8500d1a2e",
+    "url": "/first-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af34cceedbfcdc21d090",
+    "url": "/first-react-app/static/js/2.72f5ee51.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/first-react-app/static/js/2.72f5ee51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d50065ed4e8500d1a2e",
+    "url": "/first-react-app/static/js/main.901fdcc3.chunk.js"
+  },
+  {
+    "revision": "65675e8b4cd474b99aeb",
+    "url": "/first-react-app/static/js/runtime-main.c4cccb17.js"
+  }
+]);
